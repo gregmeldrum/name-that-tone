ab03a675e814aa433f6078109334a624cab5022d
gregmeldrum@gmail.com <greg.meldrum@solace.com>
2018-11-10 22:36:48 -0500

    Add new files

---
+++ js/app/app.js
@@ -0,0 +1,102 @@
+var notes = [];
+notes[0] = [0, 4, 7];  //major
+notes[1] = [0, 3, 7];  //minor
+notes[2] = [0, 4, 8];  //augmented
+notes[3] = [0, 3, 6];  //diminished
+
+var triadNames = ["major", "minor", "augmented", "diminished"];
+
+var currentTriad;
+var currentTonic;
+
+var loading = true;
+
+window.onload = function () {
+    MIDI.loadPlugin({
+        soundfontUrl: "./soundfont/",
+        instrument: "acoustic_grand_piano",
+        onprogress: function(state, progress) {
+            console.log("Progress ", state, progress);
+        },
+        onsuccess: function() {
+            document.getElementById("loading").style.display = "none";
+            document.getElementById("playTriadButton").style.display = "inline";
+        }
+    });
+};
+
+function playTriad() {
+    hideElement("playTriadButton");
+    hideElement("answer");
+    
+    // Choose a random triad and tonic note
+    currentTriad = Math.floor((Math.random() * 4));
+    currentTonic = Math.floor((Math.random() * 20) + 45);
+
+    playNotes(currentTonic, notes[currentTriad]);
+}
+
+function replay() {
+    playNotes(currentTonic, notes[currentTriad]);
+    hideElement("replay");
+}
+
+function playNotes(baseNote, noteOffsets) {
+
+    var delay = 0; // play one note every quarter second
+    var velocity = 127; // how hard the note hits
+
+    // play the note
+    MIDI.setVolume(0, 127);
+    
+    // Start and end each note
+    noteOffsets.forEach(function(note) {
+        console.log("Playing note ", note);
+        MIDI.noteOn(0, baseNote + note, velocity, delay);
+        MIDI.noteOff(0, baseNote + note, delay + 0.75);
+    });
+
+    showElement("guess");
+    showElement("replay");
+}
+ 
+function guessMajor() {
+   checkAnswer(0);
+}
+
+function guessMinor() {
+    checkAnswer(1);
+}
+
+function guessAugmented() {
+    checkAnswer(2);
+}
+
+function guessDiminished() {
+    checkAnswer(3);
+}
+
+function checkAnswer(guess) {
+    hideElement("guess");
+    showElement("answer");
+
+    if (guess === currentTriad) {
+        document.getElementById("answer").innerHTML = "<p>Correct! " + triadNames[currentTriad] + "</p>";
+    } else {
+        document.getElementById("answer").innerHTML = "<p>Incorrect :(<br>Actual Triad: " + triadNames[currentTriad] + "</p>";
+    }
+
+    showElement("playTriadButton");
+}
+
+function showElement(elementId) {
+    updateDisplayCssForElement(elementId, "inline");
+}
+
+function hideElement(elementId) {
+    updateDisplayCssForElement(elementId, "none");
+}
+
+function updateDisplayCssForElement(elementId, displayStyle) {
+   document.getElementById(elementId).style.display = displayStyle;
+}
